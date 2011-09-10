c24e0b776f59fe185cbeb592eb05c0842df6bd9e
hui <fortez@gmail.com>
2011-09-10 13:30:04 +0800

    fix image link

--- lib/simple_captcha/view.rb
+++ lib/simple_captcha/view.rb
@@ -45,13 +45,13 @@ module SimpleCaptcha #:nodoc
     def show_simple_captcha(options={})
       key = simple_captcha_key(options[:object])
       options[:field_value] = set_simple_captcha_data(key, options)
-      
+
       defaults = {
          :image => simple_captcha_image(key, options),
          :label => options[:label] || I18n.t('simple_captcha.label'),
          :field => simple_captcha_field(options)
          }
-         
+
       render :partial => 'simple_captcha/simple_captcha', :locals => { :simple_captcha_options => defaults }
     end
 
@@ -60,13 +60,13 @@ module SimpleCaptcha #:nodoc
       def simple_captcha_image(simple_captcha_key, options = {})
         defaults = {}
         defaults[:time] = options[:time] || Time.now.to_i
-        
+
         query = defaults.collect{ |key, value| "#{key}=#{value}" }.join('&')
-        url = "/simple_captcha/#{simple_captcha_key}?#{query}"
-        
+        url = "simple_captcha/#{simple_captcha_key}?#{query}"
+
         "<img src='#{url}' alt='captcha' />".html_safe
       end
-      
+
       def simple_captcha_field(options={})
         if options[:object]
           text_field(options[:object], :captcha, :value => '', :autocomplete => 'off') +
@@ -78,33 +78,33 @@ module SimpleCaptcha #:nodoc
 
       def set_simple_captcha_data(key, options={})
         code_type = options[:code_type]
-        
+
         value = generate_simple_captcha_data(code_type)
         data = SimpleCaptcha::SimpleCaptchaData.get_data(key)
         data.value = value
         data.save
         key
       end
-   
+
       def generate_simple_captcha_data(code)
         value = ''
-        
+
         case code
-          when 'numeric' then 
+          when 'numeric' then
             SimpleCaptcha.length.times{value << (48 + rand(10)).chr}
           else
             SimpleCaptcha.length.times{value << (65 + rand(26)).chr}
         end
-        
+
         return value
       end
-      
+
       def simple_captcha_key(key_name = nil)
         if key_name.nil?
           session[:captcha] ||= SimpleCaptcha::Utils.generate_key(session[:id].to_s, 'captcha')
         else
           SimpleCaptcha::Utils.generate_key(session[:id].to_s, key_name)
         end
-      end 
+      end
   end
 end
